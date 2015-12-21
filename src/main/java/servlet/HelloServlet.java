72f0d5e3929c15571d4d89b6a67325e14e6c15cc
amrit singh bains <amrit.singh.bains@live.com>
2015-12-20 21:25:50 -0500

    simple first

--- src/main/java/servlet/HelloServlet.java
+++ src/main/java/servlet/HelloServlet.java
@@ -20,6 +20,8 @@ import javax.servlet.http.HttpServletResponse;
 import org.apache.commons.collections.MultiMap;
 import org.apache.commons.collections.map.MultiValueMap;
 
+import com.google.gson.Gson;
+
 import servlet.RedBlackBST;
 
 @WebServlet(
@@ -31,8 +33,11 @@ public class HelloServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
+    	Gson gson = new Gson();
+		List<String> matched = findingLeastEditDistance(req.getParameter("q"));
+		
         ServletOutputStream out = resp.getOutputStream();
-        out.write("hello sheroku".getBytes());
+        out.write(gson.toJson(matched).getBytes());
         out.flush();
         out.close();
     }
