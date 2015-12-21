41030adf20f374adeb6937b10392ff20b6276a9a
amrit singh bains <amrit.singh.bains@live.com>
2015-12-20 23:36:24 -0500

    yes222

--- src/main/java/servlet/HibernateUtil.java
+++ src/main/java/servlet/HibernateUtil.java
@@ -11,10 +11,10 @@ public class HibernateUtil {
     private static SessionFactory buildSessionFactory() {
         try {
             // Create the SessionFactory from hibernate.cfg.xml file:///home/amrit/Documents/playground/RESTfulExample/src/main/resources
-//            return new AnnotationConfiguration().configure()
-//                    .buildSessionFactory();
-            Configuration configuration = new Configuration().configure( "src/main/java/servlet/hibernate.cfg.xml");
-            return new Configuration().configure().buildSessionFactory();
+            return new AnnotationConfiguration().configure()
+                    .buildSessionFactory();
+//            Configuration configuration = new Configuration().configure( "src/main/java/servlet/hibernate.cfg.xml");
+//            return new Configuration().configure().buildSessionFactory();
         } catch (Throwable ex) {
             System.err.println("Initial SessionFactory creation failed." + ex);
             throw new ExceptionInInitializerError(ex);
